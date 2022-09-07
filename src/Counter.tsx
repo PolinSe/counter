da92617e7fe9322fd79fb5a00cd27fd56f1b1eff
Polina Sedunova <lina.malina.spb@gmail.com>
2022-09-07 11:30:35 +0300

    refactor: refactor counter

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,45 +1,110 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './Counter.css';
-import {Display} from './Display';
-import {UniversalButton} from './UniversalButton';
+import {Button} from './components/Button';
 
 function Counter() {
 
-    const minValue = 0
-    const maxValue = 5
+    const [startValue, setStartValue] = useState<number>(0)
+    const [maxValue, setMaxValue] = useState<number>(7)
 
-    const [value, setValue] = useState<number>(minValue)
+    const [value, setValue] = useState<number>(startValue)
+    const [error, setError] = useState<string | null>(null)
+
+    const [mode, setMode] = useState<boolean>(true)
+    // mode === false - режим текста
+    // mode === true - режим счетчика (видим значение)
+
 
     const increaseValue = () => {
-        if (value < maxValue) setValue(value + 1)
+        if (value < maxValue) {
+            setValue(value + 1)
+        }
         else return
     }
 
-    const resetValue = () => setValue(minValue)
+    const resetValue = () => setValue(startValue)
+
+
+    const setParamsCallBack = () => {
+        setValue(startValue)
+        setMode(true)
+    }
+
+    const changeMaxValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.currentTarget.value) <= startValue) {
+            setError('Invalid value!')
+        } else setError('')
+
+        setMode(false)
+        setMaxValue(Number(e.currentTarget.value))
+    }
+
+    const changeStartValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        if (Number(e.currentTarget.value) < 0 || Number(e.currentTarget.value) >= maxValue) {
+            setError('Invalid value!')
+        } else setError('')
+
+        setMode(false)
+        setStartValue(Number(e.currentTarget.value))
+    }
 
     return (
-        <div className="counter">
+        <div className="app">
 
-            <Display
-                className={value < maxValue ? 'display' : 'display red'}
-                value={value}
-            />
+            <div className="counter counter_settings">
 
-            <div className="buttons">
-                <UniversalButton
-                    className={value < maxValue ? 'btn' : 'btn btn-disabled'}
-                    title={'inc'}
-                    value={value}
-                    callBack={increaseValue}
-                    disabled={value >= maxValue}
-                />
-                <UniversalButton
-                    className={value === minValue ? 'btn btn-disabled' : 'btn'}
-                    title={'reset'}
-                    value={value}
-                    callBack={resetValue}
-                    disabled={value === minValue}
-                />
+                <div className={'display'}>
+                    <label className={'text'}>max value:
+                        <input className={startValue >= maxValue ? 'input borderRed' : 'input'} value={maxValue}
+                               onChange={changeMaxValueHandler} type="number"/>
+                    </label>
+                    <label className={'text'}>start value:
+                        <input className={startValue >= maxValue || startValue < 0 ? 'input borderRed' : 'input'}
+                               value={startValue} onChange={changeStartValueHandler} type="number"/>
+                    </label>
+                </div>
+
+                <div className="buttons">
+                    <Button
+                        className={(startValue >= maxValue || startValue < 0) || mode === true ? 'btn btn-disabled' : 'btn'}
+                        title={'set'}
+                        value={value}
+                        callBack={setParamsCallBack}
+                        disabled={startValue >= maxValue || startValue < 0 || maxValue < 0 || mode === true}/>
+                </div>
+            </div>
+
+            <div className="counter counter_dashboard">
+                {
+                    !mode
+                        ? <div className={value < maxValue ? 'display value' : 'display value red'}>
+                            {error
+                                ? <div className={'display error red'}>{error}</div>
+
+                                : <div className={'display instructions '}>enter values and press 'set'</div>
+
+                            }
+                        </div>
+                        : <div className={value < maxValue ? 'display value' : 'display value red'}>{value}</div>
+                }
+
+
+                <div className="buttons">
+                    <Button
+                        className={value >= maxValue || mode === false ? 'btn btn-disabled' : 'btn '}
+                        title={'inc'}
+                        value={value}
+                        callBack={increaseValue}
+                        disabled={value >= maxValue || mode === false}
+                    />
+                    <Button
+                        className={value === startValue || mode === false ? 'btn btn-disabled' : 'btn'}
+                        title={'reset'}
+                        value={value}
+                        callBack={resetValue}
+                        disabled={value === startValue || startValue < 0 || mode === false}
+                    />
+                </div>
             </div>
         </div>
     );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-type UniversalButtonPropsType = {
+type ButtonPropsType = {
     title: string
     value: number
     callBack: () => void
     disabled?: boolean
     className: string
 }
-export const UniversalButton: React.FC<UniversalButtonPropsType> = ({title, value, callBack, disabled, className}) => {
+export const Button: React.FC<ButtonPropsType> = ({title, value, callBack, disabled, className}) => {
     return (
         <button
             className={className}
